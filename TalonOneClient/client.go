226189b6a8e47c0c91316d94d3a073bf8f0f6438
alex <zyxancf@gmail.com>
2017-10-02 16:49:19 +0200

    lolprints

--- TalonOneClient/client.go
+++ TalonOneClient/client.go
@@ -4,15 +4,15 @@ import "fmt"
 
 // UpdateCustomerProfile prints some stuff to make you feel realized.
 func UpdateCustomerProfile() {
-	fmt.Println("Look, I've been imported!")
+	fmt.Println("I've been told to update the customer profile ...")
 }
 
 // UpdateCustomerSession prints some stuff to make you feel realized.
 func UpdateCustomerSession() {
-	fmt.Println("none of yo' business")
+	fmt.Println("this session is none of yo' business homie!")
 }
 
 // SendEvents prints some stuff to make you feel realized.
 func SendEvents() {
-	fmt.Println("look a baloon -> 🎈")
+	fmt.Println("look a flying baloon -> 🎈")
 }
