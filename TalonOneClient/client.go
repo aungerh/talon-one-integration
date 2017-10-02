9a096ab4bf5f8d7c50a51966c834501595f589ae
alex <zyxancf@gmail.com>
2017-10-02 16:45:48 +0200

    some more exported functionality

--- TalonOneClient/client.go
+++ TalonOneClient/client.go
@@ -2,7 +2,17 @@ package TalonOneClient
 
 import "fmt"
 
-// ExportedFunc prints some stuff to make you feel realized.
-func ExportedFunc() {
+// UpdateCustomerProfile prints some stuff to make you feel realized.
+func UpdateCustomerProfile() {
 	fmt.Println("Look, I've been imported!")
+}
+
+// UpdateCustomerSession prints some stuff to make you feel realized.
+func UpdateCustomerSession() {
+	fmt.Println("none of yo' business")
+}
+
+// SendEvents prints some stuff to make you feel realized.
+func SendEvents() {
+	fmt.Println("look a baloon -> 🎈")
 }
