9fc8c9004b54eab53832ae6356e0d0ce8e3483f7
alex <zyxancf@gmail.com>
2017-10-04 10:35:44 +0200

    code style

--- TalonOneClient/integration.go
+++ TalonOneClient/integration.go
@@ -16,11 +16,11 @@ func (p *Payload) updateCustomerProfile() {
 	profileName := "tronald_dump"
 	url := dest + profileName
 	js, _ := json.Marshal(*p)
+
 	p.Attributes.Gender = "m"
 
 	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer(js))
 	signatureVal := fmt.Sprintf("signer=%d; signature=%s", 60, signPayload(js))
-	fmt.Printf("headers: %s \n", signatureVal)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Content-Signature", signatureVal)
 
