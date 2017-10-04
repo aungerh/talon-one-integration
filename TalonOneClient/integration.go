59005fdaeb6464f22915f88c0cf853d1a69a1bea
alex <zyxancf@gmail.com>
2017-10-04 10:46:46 +0200

    modularity

--- TalonOneClient/integration.go
+++ TalonOneClient/integration.go
@@ -1,32 +1,13 @@
 package TalonOneClient
 
 import (
-	"bytes"
 	"crypto/hmac"
 	"crypto/md5"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
-
-// UpdateCustomerProfile updates customer profile
-func (p *Payload) UpdateCustomerProfile() {
-	dest := "https://demo.talon.one/v1/customer_profiles/"
-	profileName := "tronald_dump"
-	url := dest + profileName
-	js, _ := json.Marshal(*p)
-
-	p.Attributes.Gender = "m"
-
-	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer(js))
-	signatureVal := fmt.Sprintf("signer=%d; signature=%s", 60, signPayload(js))
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Content-Signature", signatureVal)
-
-	doRequest(req)
-}
 
 func signPayload(body []byte) string {
 	const s = "4aece57f964f4784"
--- TalonOneClient/payload.go
+++ TalonOneClient/payload.go
@@ -1,5 +1,12 @@
 package TalonOneClient
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // Payload represents the body of the request
 type Payload struct {
 	Attributes attributes `json:"attributes"`
@@ -7,4 +14,21 @@ type Payload struct {
 
 type attributes struct {
 	Gender string `json:"Gender,omitempty"`
+}
+
+// UpdateCustomerProfile updates customer profile
+func (p *Payload) UpdateCustomerProfile() {
+	dest := "https://demo.talon.one/v1/customer_profiles/"
+	profileName := "tronald_dump"
+	url := dest + profileName
+	js, _ := json.Marshal(*p)
+
+	p.Attributes.Gender = "m"
+
+	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer(js))
+	signatureVal := fmt.Sprintf("signer=%d; signature=%s", 60, signPayload(js))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Signature", signatureVal)
+
+	doRequest(req)
 }
