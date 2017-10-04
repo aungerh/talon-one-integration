8096e5f068f68ecfbda5b4d6066cb064a7b04ff4
alex <zyxancf@gmail.com>
2017-10-04 15:14:11 +0200

    url typo

--- TalonOneClient/integration.go
+++ TalonOneClient/integration.go
@@ -3,5 +3,5 @@ package TalonOneClient
 const (
 	customerProfileEndpoint  = "https://demo.talon.one/v1/customer_profiles/"
 	customerSessionsEndpoint = "https://demo.talon.one/v1/customer_sessions/"
-	eventsEndpoint           = "https://demo.talon.one/v1/events/"
+	eventsEndpoint           = "https://demo.talon.one/v1/events"
 )
