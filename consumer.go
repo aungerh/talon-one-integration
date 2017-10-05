1a8de2d588e8389081dadfe33f8fd8bad5850a78
alex <zyxancf@gmail.com>
2017-10-05 12:28:01 +0200

    randomize session URLParams

--- consumer.go
+++ consumer.go
@@ -1,13 +1,38 @@
 package main
 
-import "github.com/zyxan/talon-one-integration/TalonOneClient"
+import (
+	"math/rand"
+	"time"
+
+	"github.com/zyxan/talon-one-integration/TalonOneClient"
+)
+
+var r *rand.Rand
+
+func init() {
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+// RandomString pretty much honors its name
+func RandomString(strlen int) string {
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := ""
+	for i := 0; i < strlen; i++ {
+		index := r.Intn(len(chars))
+		result += chars[index : index+1]
+	}
+	return result
+}
 
 func main() {
 	client := TalonOneClient.NewClient(60, "4aece57f964f4784")
+	var str = RandomString(10)
 
 	client.Payload = TalonOneClient.Payload{
 		URLParams: "tronald_dump",
 		Attributes: TalonOneClient.Attributes{
+			// triggers a rule in campaign #1
 			Name: "Trump",
 		},
 	}
@@ -18,13 +43,15 @@ func main() {
 		SessionID: "tronald_dump",
 		Type:      "dangerous",
 		Attributes: TalonOneClient.Attributes{
+			// triggers a rule in campaign #3
 			VeryDangerous: "i_am_the_one_who_knocks",
 		},
 	}
 	client.SendEvents()
 
 	client.Payload = TalonOneClient.Payload{
-		URLParams:  "GoCLISession1",
+		// triggers a rule in campaign #2, onSessionCreate
+		URLParams:  "session_" + str,
 		ProfileID:  "306",
 		State:      "open",
 		Total:      200,
